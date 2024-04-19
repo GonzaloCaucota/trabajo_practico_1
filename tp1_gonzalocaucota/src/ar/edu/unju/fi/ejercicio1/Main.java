14c8417b69e26b3598fa1bf88dc6cc61fdc27103
gonza <gonza@Envy>
2024-04-18 22:30:52 -0300

    modificacion ejercicio 1

--- tp1_gonzalocaucota/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_gonzalocaucota/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num1 = 10;
+		int num2 = 9;
+		int num3 = 8;
+		int num4 = 7;
+		int num5 = 6;
+		
+		// calcular promedio+
+		
+		double promedio	= (num1+ num2 + num3+ num4 + num5)/ 5.0;
+		
+		System.out.println("el promedio es " + promedio);
 		System.out.println("bienvenido a programacion visual!!");
 
 	}
